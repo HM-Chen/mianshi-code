988838362ed3149a21c0b13a8fe7e4aab6376f95
Homes <Homes@mai.com>
2022-04-05 17:16:24 +0800

    first commit second commit hot-fix commit hot-fix conflict commit master conflict commit

--- meituan/src/com/meituan/fuzaijunheng.java
+++ meituan/src/com/meituan/fuzaijunheng.java
@@ -5,6 +5,7 @@ public class fuzaijunheng {
     public static void main(String[] args){
         System.out.println("hello git");
         System.out.println("hello hot-fix");
+        System.out.println("hello conflict2");
         Scanner in = new Scanner(System.in);
         int n = in.nextInt();
         int[] times = new int[n];
